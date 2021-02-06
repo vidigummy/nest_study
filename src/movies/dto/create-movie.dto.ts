8e2e0f82217b72c45579bea9a43314a114aa2d78
vidigummy <vidigummy@gmail.com>
2021-02-06 14:10:27 +0900

    h

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,5 +1,11 @@
+import {isNumber, isString, IsString} from 'class-validator';
+
+
 export class CreateMovieDto{
+    @IsString()
     readonly title: string;
+    @IsNumber()
     readonly year: number;
+    @IsString( {each:true })
     readonly genres: string[];
 }
