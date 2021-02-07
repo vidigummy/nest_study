b2f2be7e29b2c22c2eb4e1f98ab8d0b1ab035ae6
vidigummy <vidigummy@gmail.com>
2021-02-07 21:46:12 +0900

    마무리

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,8 +17,15 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/movies')
       .expect(200)
-      .expect('Hello World!');
+      .expect('씨발!');
+  });
+
+  it("/movies (GET)", () =>{
+    return request(app.getHttpServer())
+      .get("/movies")
+      .expect(200)
+      .expect('[]');
   });
 });
