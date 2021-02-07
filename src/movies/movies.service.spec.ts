026f31121f8ba81959a2a6778d42d50c4b0be833
vidigummy <vidigummy@gmail.com>
2021-02-07 20:59:42 +0900

    hihi

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -43,4 +43,41 @@ describe('MoviesService', () => {
       }
     });
   });
+
+
+  describe("deleteOne", ()=>{
+    it("deletes a movie", () =>{
+      service.create({
+        title:"Test Movie",
+        year:2021,
+        genres:["test"],
+      });
+      const am = service.getAll();
+      service.deleteOne(1);
+      const ad = service.getAll();
+      expect(ad.length).toBeLessThan(am.length);
+    });
+    it("it sould return 404 error", () =>{
+      try{
+        service.deleteOne(9999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+
+
+  describe("create",()=>{
+    it("should create a movie", ()=>{
+      const bc= service.getAll().length;
+      service.create({
+        title:"Test Movie",
+        year:2021,
+        genres:["test"],
+      });
+      const ac = service.getAll().length;
+      expect(ac).toBeGreaterThan(bc);
+    });
+  });
 });
