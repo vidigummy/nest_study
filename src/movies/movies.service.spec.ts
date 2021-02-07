0894867b0738da0dbc5bac11358bb67d4b6b0250
vidigummy <vidigummy@gmail.com>
2021-02-07 20:19:59 +0900

    test2

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -16,7 +17,30 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  it("should be 4", ()=>{
-    expect(2+3).toEqual(5);
+  describe("getAll", () =>{
+    it("should return an array",()=>{
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe("getOne",()=>{
+    it('should return a movie', ()=>{
+      service.create({
+        title:"Test Movie",
+        year:2021,
+        genres:["test"],
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it("should throw 404 error", () =>{
+      try{
+        service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
