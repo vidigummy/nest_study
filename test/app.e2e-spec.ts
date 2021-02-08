2dc331541f65578fe97dcff99c25c176281e9a29
vidigummy <vidigummy@gmail.com>
2021-02-09 00:13:13 +0900

    hi.....

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -29,6 +36,16 @@ describe('AppController (e2e)', () => {
     it('POST', () =>{
       return request(app.getHttpServer()).post("/movies").send({title:"test",year:2020,genres:['test'],}).expect(201);
     });
+
+    it("Delete", ()=>{
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
-  
+  describe('/movies/:id', () =>{
+    it('GET 404', () =>{
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it.todo("DELETE");
+    it.todo("PATCH");
+  });
 });
